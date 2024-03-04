6e3eb6df6621120d538f2a24345f057303f753a0
ivsvat <iv.svatko@gmail.com>
2024-03-04 13:48:30 +0100

    first

--- src/train.py
+++ src/train.py
@@ -1,5 +1,14 @@
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
+import numpy as np
+from tqdm import tqdm
+from sklearn.ensemble import RandomForestRegressor
+import copy
+import random
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from joblib import dump, load
 
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
@@ -10,12 +19,71 @@ env = TimeLimit(
 # You have to implement your own agent.
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
+# class ProjectAgent:
+#     def act(self, observation, use_random=False):
+#         return 0
+
+#     def save(self, path):
+#         pass
+
+#     def load(self):
+#         pass
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity # capacity of the buffer
+        self.data = []
+        self.index = 0 # index of the next cell to be filled
+    def append(self, s, a, r, s_, d):
+        if len(self.data) < self.capacity:
+            self.data.append(None)
+        self.data[self.index] = (s, a, r, s_, d)
+        self.index = (self.index + 1) % self.capacity
+    def sample(self, batch_size):
+        batch = random.sample(self.data, batch_size)
+        return list(map(lambda x:torch.Tensor(np.array(x)), list(zip(*batch))))
+    def __len__(self):
+        return len(self.data)
+    
+state_dim = env.observation_space.shape[0]
+n_action = env.action_space.n 
+nb_neurons=36
+DQN = torch.nn.Sequential(nn.Linear(state_dim, nb_neurons),
+
+                          nn.ReLU(),
+                          nn.Linear(nb_neurons, nb_neurons),
+                          nn.ReLU(),
+                          nn.Linear(nb_neurons, nb_neurons),
+                          nn.ReLU(),
+                          nn.Linear(nb_neurons, n_action))
+
+# class ProjectAgent:
+#     def __init__(self):
+#         self.model = DQN
+
+#     def act(self, observation, use_random=False):
+#         return torch.argmax(self.model(torch.FloatTensor(observation)).unsqueeze(0)).item()
+
+#     def save(self, path):
+#         torch.save(self.model.state_dict(), r"best.pt")
+#     def load(self):
+#         self.model.load_state_dict(torch.load(r"best.pt"))
+
 class ProjectAgent:
+    def __init__(self):
+        self.model = None
+
     def act(self, observation, use_random=False):
-        return 0
+        Qsa =[]
+        for a in range(env.action_space.n):
+            sa = np.append(observation,a).reshape(1, -1)
+            Qsa.append(self.model.predict(sa))
+        a = np.argmax(Qsa)
+        return a
 
     def save(self, path):
         pass
-
+        # torch.save(self.model.state_dict(), r"best.pt")
     def load(self):
-        pass
+        self.model = load("samples\Q6")
+        print(f"Using {self.model}")
